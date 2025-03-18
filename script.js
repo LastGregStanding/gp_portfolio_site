af87c30217a0debfa9f080f83d05da6c802ee593
Greg Preiss <gregory.preiss@gmail.com>
2025-03-18 12:32:26 -0600

    added a nav and hamburger menu

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
 "use strict";
+
+document.addEventListener("DOMContentLoaded", () => {
+  const hamburgerMenu = document.querySelector(".hamburger-menu");
+  const hamburgerBtn = document.querySelector(".hamburger-btn");
+  const menuBtn = document.querySelectorAll(".menu-btn");
+  const body = document.body;
+
+  // Open Menu
+  hamburgerBtn.addEventListener("click", () => {
+    hamburgerMenu.classList.add("active");
+    body.classList.add("no-scroll"); // Prevent scrolling
+  });
+
+  menuBtn.forEach((btn) => {
+    btn.addEventListener("click", () => {
+      hamburgerMenu.classList.remove("active");
+      body.classList.remove("no-scroll"); // Enable scrolling
+    });
+  });
+});
